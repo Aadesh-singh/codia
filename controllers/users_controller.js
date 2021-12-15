b073caead138edcefa6f391e61c13ebcc66112f5
Aadesh-singh <aadeshsingh146@gmail.com>
2021-12-15 16:49:11 +0530

    Profile Upload

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,7 @@
 // importing model
 const User = require('../models/user');
-
+const fs = require('fs');
+const path = require('path');
 
 
 // Rendering the Profiles page.
@@ -78,6 +79,10 @@ module.exports.update = async function(req, res){
                 user.email = req.body.email;
                  
                 if(req.file){
+                    if(user.avatar){
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                    }
+
                     // this is saving the path of uploaded file into the avatar field in the user
                     user.avatar = User.avatarPath + '/' + req.file.filename;
                 }
