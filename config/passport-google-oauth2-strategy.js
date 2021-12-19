ffa73214c390c3c86520cce4b6b0fa58901753c9
Aadesh-singh <aadeshsingh146@gmail.com>
2021-12-19 08:10:46 +0530

    Authentication using google

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,45 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+const User = require('../models/user');
+
+
+// Tell passport to use new strategy from googleAuth
+passport.use(new googleStrategy({
+    // clientID and clientSecret are Provided from google itself.
+        clientID: "184780315512-s9ija3k6632p8gv5rsjjl1mvbkbmvfec.apps.googleusercontent.com",
+        clientSecret: "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",
+        callbackURL: "http://localhost:8000/user/auth/google/callback"
+    },
+    // accessToken contains the token and refresh token does is when the token gets expired instead of telling user to sign in it obtains a renewed access token.
+    function(accessToken, refreshToken, profile, done){    //Profile will contain the all user info from google
+        // find user
+        User.findOne({email: profile.emails[0].value}).exec(function(err, user){        //profile.emails[0].value ==> because google account can contain multiple emails
+            if(err){
+                console.log('Error in Google strategy-passport', err);
+                return;
+            }
+            console.log(profile);
+            // if found then set this user as req.user/sign-in the user.
+            if(user){
+                return done(null, user);
+            } else{
+                // if not found then create the user and set it to req.user
+                User.create({
+                    name: profile.displayName,          //displayName is Key for name of user
+                    email: profile.emails[0].value,
+                    password: crypto.randomBytes(20).toString('hex')        //this line will use crypto module to create random hexadecimal passwords because password is a required field and it cannot be empty.
+                }, function(err, user){
+                    if(err){
+                        console.log('Error in creating user: Google strategy-passport', err);
+                        return;
+                    }
+                    return done(null, user);
+                });
+            }
+        });
+    }
+
+));
+
+module.exports = passport;
--- routes/user.js
+++ routes/user.js
@@ -30,4 +30,9 @@ router.post('/update/:id', passport.checkAuthentication, userController.update);
 // sign-out
 router.get('/sign-out', userController.destroySession);
 
+
+// Google Sign-IN routers
+router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/user/sign-in'}), userController.createSession);
+
 module.exports = router;
